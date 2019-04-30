5deba638f348b80713993830950944fbfafef1af
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-04-30 18:30:15 +0000

    [FIX] fix code point 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ elif lastDigit > 5:
     print("Last digit of {:d} is {:d} and is grater \
 than 5".format(number, lastDigit))
 elif lastDigit < 6 and lastDigit != 0:
-    print("Last digit of {:d} is {:d} and is less than \
+    print("Last digit of {: d} is {: d} and is less than \
 6 and not 0".format(number, lastDigit))
