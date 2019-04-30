ffe5d8236ccb6ef60eff9c6d7e57152e2235d612
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-04-30 19:23:57 +0000

    [FIX] code correction

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alph in range(97, 122):
+for alph in range(97, 123):
     print("{:s}".format(chr(alph)), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for alph in range(97, 122):
+for alph in range(97, 123):
     if alph == 101 or alph == 113:
         continue
     print("{:s}".format(chr(alph)), end='')
