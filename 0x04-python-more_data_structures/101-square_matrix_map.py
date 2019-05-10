0b2a0709862943333ebff4a3fbd31bc8b8beb369
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-05-10 02:57:19 +0000

    [FIX] code correction

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda elem, y: elem*y, row, row)), matrix))
+    return list(map(lambda i: list(map(lambda j, x: j * x, i, i)), matrix))
