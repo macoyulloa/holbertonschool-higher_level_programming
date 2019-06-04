b4e931a37d7e7300ac5c953f8ca2386387e5f66a
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-06-04 21:41:39 +0000

    [FILES] uploading the exercises of teh directory 0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def lookup(obj):
+    lista = []
+    lista = dir(obj)
+    return lista
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+class BaseGeometry:
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        nomb = type(self).__name__
+        pepito = "[{}] {:d}/{:d}".format(nomb, self.__width, self.__height)
+        return(pepito)
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        super().__init__(size, size)
