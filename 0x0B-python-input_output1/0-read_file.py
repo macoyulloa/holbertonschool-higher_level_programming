019fe5e26be4256f643a861c097c4768e6f45a5d
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-06-05 22:20:04 +0000

    []

--- 0x0B-python-input_output1/0-read_file.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def read_file(filename=""):
-    with open(filename, encoding="UTF8") as myfile:
-        for line in myfile:
-            print(line, end='')
--- 0x0B-python-input_output1/7-save_to_json_file.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    with open(filename, "w") as myfile:
-        json.dump(my_obj, myfile)
