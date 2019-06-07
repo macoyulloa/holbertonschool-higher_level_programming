6429e63e2b5103f7d4240a507cd09f17449c2ffd
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-06-07 15:55:00 +0000

    [FIX] correction point 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square():
     def __init__(self, size=0):
-        if not type(size) == int:
+        if type(size) != int:
             raise TypeError("size must be and integer")
         elif size < 0:
             raise ValueError('size must be >= 0')
