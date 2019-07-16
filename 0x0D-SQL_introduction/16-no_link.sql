ccd2792d9fdaaa46d07af09dbbdd14775b05eb4c
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-07-16 02:50:15 +0000

    fix

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- updates the score of Bob 
 -- MySQL server
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY 1 DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
