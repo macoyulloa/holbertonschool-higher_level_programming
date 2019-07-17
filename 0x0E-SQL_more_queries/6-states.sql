f88119042fd21abee54f17514ee63f24ebe19909
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-07-17 03:25:15 +0000

    file

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates table force_name 
+-- MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+name VARCHAR(256) NOT NULL);
