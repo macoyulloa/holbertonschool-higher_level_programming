17c52696dc3a0e8b7fd339fe1da853b5e8ccbbf7
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-07-18 01:24:17 +0000

    uploading fix tool point 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -6,5 +6,5 @@ JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres 
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_genres.name ASC;
+WHERE tv_genres.name = "Comedy" 
+ORDER BY tv_shows.title ASC;
