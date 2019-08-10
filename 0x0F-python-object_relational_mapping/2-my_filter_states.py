5ccda5eab140c20a8e92914204c8c8cfa5e387e5
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-10 20:56:49 +0000

    correction point 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
             db=argv[3],
             charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name = '{}'\
+            ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
