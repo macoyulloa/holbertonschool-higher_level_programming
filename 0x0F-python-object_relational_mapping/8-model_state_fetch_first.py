9bf6aaeb0222a00f7aff1bbf1c244afa66813202
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-11 01:33:50 +0000

    uploading code point 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
-        print("")
+        print("Nothing")
     session.close()
