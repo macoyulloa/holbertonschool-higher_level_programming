15561443dfc38e480cdadb3970017c2804309b34
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-20 21:50:41 +0000

    uploading code point 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request to the passed URL 
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST $1
+curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST $1
