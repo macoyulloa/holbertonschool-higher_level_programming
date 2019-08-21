bbdb55b2acbddfaad056348a310655b0266e3271
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-21 02:16:47 +0000

    code correction advaanced task

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL
-curl -sLX PUT "" -d "user_id=98" -H "Origin: Holber tonSchool" 0.0.0.0:5000/catch_me
+curl -sLX PUT "" -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
