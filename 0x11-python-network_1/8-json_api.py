69708e080497d8644886a34b7296b402d345b266
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-27 04:49:27 +0000

    point 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+" using a urllib library "
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    " POST request, display a body and mail "
+    url = "http://0.0.0.0:5000/search_user"
+    if argv[2]:
+        values = {'q': argv[2]}
+    else:
+        values = {'q': ""}
+    req_post = requests.post(url, data=values)
+    try:
+        req = req_post.json()
+        if req == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(req.get("id"), req.get("name")))
+    except:
+        print("Not a valid JSON")
