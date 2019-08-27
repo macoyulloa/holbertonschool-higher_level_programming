c39061b4d3190c6225bce89e94d470224c6090a0
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-08-27 00:08:54 +0000

    uploading code point 2, POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+" using a urllib library "
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    " POST request, display a body and mail "
+    values = {'email': 'argv[2]'}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print(page.decode("utf-8"))
