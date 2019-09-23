b6f447653e0a56df79133ebcf88cfa97bcd1e02a
Maria Alejandra Coy Ulloa <764@holbertonschool.com>
2019-09-23 17:58:04 +0000

    [FILE] uploading code point 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,17 +4,14 @@ const SquareF = require('./5-square');
 module.exports = class Square extends SquareF {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   charPrint (c) {
     if (c === undefined) {
-      for (let i = 0; i < this.size; i++) {
-        console.log('X'.repeat(this.size));
-      }
+	this.print();
     } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
       }
     }
   }
